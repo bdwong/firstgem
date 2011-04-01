4044a012134a7a2f93cc5238e3bb63a75e0c18f8
Brian Wong <bdwong.net@gmail.com>
2011-04-01 12:40:49 -0700

    Include stdc++ library for linux x86_64

--- ext/firstgem/extconf.rb
+++ ext/firstgem/extconf.rb
@@ -1,6 +1,12 @@
 require 'mkmf'
 # Note: It's not a good idea to name the extension the same as the module,
-# otherwise it will be more difficult to include.
+# otherwise it will be more difficult to include. See workaround in firstgem.rb
+
+# Include standard c++ libs
+if RUBY_PLATFORM =~ /linux/ #and RbConfig::MAKEFILE_CONFIG["host_cpu"] == "x86_64"
+  have_library('stdc++')
+end
+
 create_makefile('firstgem')
 
 # Create wrapper target so it's easier to make the extension.
